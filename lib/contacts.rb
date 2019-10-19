a4800569cbc9918dd3fc57d79402dce114e5db38
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-19 17:29:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
-  end
+   
     if person == "Freddy Mercury"
       contacts_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors 
